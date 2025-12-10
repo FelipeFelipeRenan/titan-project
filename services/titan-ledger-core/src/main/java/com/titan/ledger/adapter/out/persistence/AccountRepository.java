01e67c610094f40d455e606a6dd2b7ab08f881ee
FelipeFelipeRenan.com <feliperenanqwerty@gmail.com>
2025-12-10 15:48:26 -0300

    feat: adding pessimistic lock to account repository and id ordenation to transfer service, to avoid race conditions

--- services/titan-ledger-core/src/main/java/com/titan/ledger/adapter/out/persistence/AccountRepository.java
+++ services/titan-ledger-core/src/main/java/com/titan/ledger/adapter/out/persistence/AccountRepository.java
@@ -4,13 +4,22 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.titan.ledger.core.domain.model.Account;
+
+import jakarta.persistence.LockModeType;
 
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID>{
         
     Optional<Account> findByClientIdAndCurrency(String clientId, String currency);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a WHERE a.id = :id")
+    Optional<Account> findByIdForUpdate(@Param("id") UUID id);
 }
--- services/titan-ledger-core/src/main/java/com/titan/ledger/core/service/TransferService.java
+++ services/titan-ledger-core/src/main/java/com/titan/ledger/core/service/TransferService.java
@@ -20,77 +20,92 @@ import com.titan.ledger.core.usecase.dto.TransferFundsCommand;
 @Service
 public class TransferService implements TransferFundsUseCase {
 
-    private final AccountRepository accountRepository;
-    private final TransactionRepository transactionRepository;
-    private final LedgerRepository ledgerRepository;
+        private final AccountRepository accountRepository;
+        private final TransactionRepository transactionRepository;
+        private final LedgerRepository ledgerRepository;
 
-    public TransferService(AccountRepository accountRepository, TransactionRepository transactionRepository,
-            LedgerRepository ledgerRepository) {
-        this.accountRepository = accountRepository;
-        this.transactionRepository = transactionRepository;
-        this.ledgerRepository = ledgerRepository;
-    }
-
-    @Override
-    @Transactional
-    public UUID execute(TransferFundsCommand command) {
-
-        // Carregar as contas
-        // TODO: Usar lock perssimista para evitar possivel race condition
-        Account fromAccount = accountRepository.findById(command.fromAccountId())
-                .orElseThrow(() -> new IllegalArgumentException("Source account not found"));
-
-        Account toAccount = accountRepository.findById(command.toAccountId())
-                .orElseThrow(() -> new IllegalArgumentException("Target account not found"));
-
-        // Validar saldo da conta que envia
-        if (fromAccount.getBalance().compareTo(command.amount()) < 0) {
-            throw new IllegalStateException("Insufficient funds");
+        public TransferService(AccountRepository accountRepository, TransactionRepository transactionRepository,
+                        LedgerRepository ledgerRepository) {
+                this.accountRepository = accountRepository;
+                this.transactionRepository = transactionRepository;
+                this.ledgerRepository = ledgerRepository;
         }
 
-        // Criar header da transação
-        Transaction transaction = new Transaction(
-                UUID.randomUUID().toString(), // Correlation ID
-                command.description());
+        @Override
+        @Transactional
+        public UUID execute(TransferFundsCommand command) {
 
-        transaction.setStatus(TransactionStatus.COMPLETED); // Status ja completo, pois no momento é sincrono
-        transactionRepository.save(transaction);
+                // DEADLOCK PREVEMTION - ordenar locks por UUID
+                UUID firstLockId = command.fromAccountId().compareTo(command.toAccountId()) < 0
+                                ? command.fromAccountId()
+                                : command.toAccountId();
 
-        // Lógica de negocio: atualizar saldos em memoria
-        BigDecimal newSourceBalance = fromAccount.getBalance().subtract(command.amount());
-        BigDecimal newTargetBalance = toAccount.getBalance().add(command.amount());
+                UUID secondLockId = command.fromAccountId().compareTo(command.toAccountId()) < 0
+                                ? command.toAccountId()
+                                : command.fromAccountId();
 
-        fromAccount.setBalance(newSourceBalance);
-        toAccount.setBalance(newTargetBalance);
+                
+                // Carregar as contas
+                Account account1 = accountRepository.findByIdForUpdate(firstLockId)
+                                .orElseThrow(() -> new IllegalArgumentException("Source account not found"));
 
-        // salvar contas atualizadas
-        accountRepository.save(fromAccount);
-        accountRepository.save(toAccount);
-        // Livro razao (ledger)
+                Account account2 = accountRepository.findByIdForUpdate(secondLockId)
+                                .orElseThrow(() -> new IllegalArgumentException("Target account not found"));
 
-        // Entrada de debito
-        LedgerEntry debitEntry = new LedgerEntry(
-                transaction,
-                fromAccount,
-                OperationType.DEBIT,
-                command.amount(),
-                newSourceBalance // snapshot do saldo pós operação
-        );
 
-        // Entrada de credito
-        LedgerEntry creditEntry = new LedgerEntry(
-                transaction,
-                toAccount,
-                OperationType.CREDIT,
-                command.amount(),
-                newTargetBalance // snapshot do saldo pós operação
-        );
+                Account fromAccount = command.fromAccountId().equals(account1.getId()) ? account1: account2;
 
-        ledgerRepository.save(debitEntry);
-        ledgerRepository.save(creditEntry);
+                Account toAccount = command.toAccountId().equals(account1.getId()) ? account1: account2;
 
-        return transaction.getId();
 
-    }
+                // Validar saldo da conta que envia
+                if (fromAccount.getBalance().compareTo(command.amount()) < 0) {
+                        throw new IllegalStateException("Insufficient funds");
+                }
+
+                // Criar header da transação
+                Transaction transaction = new Transaction(
+                                UUID.randomUUID().toString(), // Correlation ID
+                                command.description());
+
+                transaction.setStatus(TransactionStatus.COMPLETED); // Status ja completo, pois no momento é sincrono
+                transactionRepository.save(transaction);
+
+                // Lógica de negocio: atualizar saldos em memoria
+                BigDecimal newSourceBalance = fromAccount.getBalance().subtract(command.amount());
+                BigDecimal newTargetBalance = toAccount.getBalance().add(command.amount());
+
+                fromAccount.setBalance(newSourceBalance);
+                toAccount.setBalance(newTargetBalance);
+
+                // salvar contas atualizadas
+                accountRepository.save(fromAccount);
+                accountRepository.save(toAccount);
+                // Livro razao (ledger)
+
+                // Entrada de debito
+                LedgerEntry debitEntry = new LedgerEntry(
+                                transaction,
+                                fromAccount,
+                                OperationType.DEBIT,
+                                command.amount(),
+                                newSourceBalance // snapshot do saldo pós operação
+                );
+
+                // Entrada de credito
+                LedgerEntry creditEntry = new LedgerEntry(
+                                transaction,
+                                toAccount,
+                                OperationType.CREDIT,
+                                command.amount(),
+                                newTargetBalance // snapshot do saldo pós operação
+                );
+
+                ledgerRepository.save(debitEntry);
+                ledgerRepository.save(creditEntry);
+
+                return transaction.getId();
+
+        }
 
 }
